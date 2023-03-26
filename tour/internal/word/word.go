a039097b574a586c85258b8de09b301be7e83508
liucheng <1545205264@qq.com>
2023-03-26 20:49:23 +0800

    tour commit

---
+++ tour/internal/word/word.go
@@ -0,0 +1,45 @@
+package word
+
+import (
+	"strings"
+	"unicode"
+)
+
+// 转换大小写
+func ToUpper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func ToLower(s string) string {
+	return strings.ToLower(s)
+}
+
+// 下划线转大写驼峰
+func UnderscoreToUpperCamelCase(s string) string {
+	s = strings.Replace(s, "_", " ", -1)
+	s = strings.Title(s)
+	return strings.Replace(s, " ", "", -1)
+}
+
+// 下划线转小写驼峰
+func UnderscoreToLowerCamelCase(s string) string {
+	s = UnderscoreToUpperCamelCase(s)
+	return string(unicode.ToLower(rune(s[0]))) + s[1:]
+	//复用大写驼峰方法，再将s第一个字符取出来变成小写，最后与后面的字符拼接
+}
+
+// 驼峰转下划线
+func CamelCaseToUnderscore(s string) string {
+	var output []rune
+	for i, r := range s {
+		if i == 0 {
+			output = append(output, unicode.ToLower(r))
+			continue
+		}
+		if unicode.IsUpper(r) {
+			output = append(output, '_')
+		}
+		output = append(output, unicode.ToLower(r))
+	}
+	return string(output)
+}
